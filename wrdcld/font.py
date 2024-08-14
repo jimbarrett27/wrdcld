ab042344c3591713ef0d67e4e3bc7fe46732147c
Marcus Näslund <naslundx@gmail.com>
2024-08-14 12:42:51 +0200

    Add and use a font wrapper class

--- wrdcld/font.py
+++ wrdcld/font.py
@@ -3,27 +3,32 @@ from PIL import Image, ImageDraw, ImageFont
 from wrdcld.util import get_repo_root
 
 
-def get_default_font_path():
-    return get_repo_root() / "fonts" / "OpenSans-Regular.ttf"
+class FontWrapper:
+    def __init__(self, path=None, color=(255, 255, 0)):
+        self.path = path or get_repo_root() / "fonts" / "OpenSans-Regular.ttf"
 
+    def with_size(self, fontsize: float) -> 'FreeTypeFont':
+        return ImageFont.truetype(self.path, fontsize)
+    
+    def length_of_word(self, fontsize: float, word: str) -> float:
+        font = self.with_size(fontsize=fontsize)
+        return font.getlength(word)
 
-def find_fontsize_for_width(width, word):
-    fontsize = width / 2
-    step = width / 2
+    def find_fontsize_for_width(self, width, word):
+        fontsize = width / 2
+        step = width / 2
 
-    font_path = get_default_font_path()
-    while step > 0.5:
-        step /= 2
+        while step > 0.5:
+            step /= 2
+            
+            length = self.length_of_word(fontsize=fontsize, word=word)
 
-        font = ImageFont.truetype(font_path, fontsize)
-        length = font.getlength(word)
+            if length < width:
+                fontsize += step
+            else:
+                fontsize -= step
 
-        if length < width:
-            fontsize += step
-        else:
-            fontsize -= step
-
-    return fontsize
+        return fontsize
 
 
 def draw_text(canvas, img, rectangle, word, font, rotate=False):
