bc1f18aeaab3b95357d1c3fbc7cb978eb80e1ca1
Jim Barrett <jimbarrett27@gmail.com>
2025-03-15 19:09:49 +0100

    add a unit test that wordclouds from different seeds are different

--- tests/test_wrdcld.py
+++ tests/test_wrdcld.py
@@ -15,11 +15,14 @@ def _two_images_are_equal(image1, image2):
 
 @st.composite
 def words_with_repeats_strategy(draw):
+    """
+    Strategy to generate a list of words with repeats
+    """
     words = draw(
         st.lists(
             st.text(ascii_letters + digits + punctuation, min_size=1, max_size=10),
             min_size=1,
-            max_size=10,
+            max_size=50,
         )
     )
 
@@ -42,6 +45,9 @@ class TestWordCloud(TestCase):
 
     @given(words=words_with_repeats_strategy())
     def test_make_word_cloud(self, words: list[str]):
+        """
+        Test that a word cloud is created
+        """
 
         background_color = (0, 0, 0)
         word_cloud = make_word_cloud(words, background_color=background_color)
@@ -57,9 +63,28 @@ class TestWordCloud(TestCase):
     @settings(deadline=None)
     @given(words=words_with_repeats_strategy(), seed=st.integers())
     def test_word_cloud_reproducibility(self, words: list[str], seed: int):
+        """
+        Test that two word clouds with the same seed are equal
+        """
         counter = Counter(words)
 
         word_cloud_1 = make_word_cloud(words, seed=seed)
         word_cloud_2 = make_word_cloud(counter, seed=seed)
 
         self.assertTrue(_two_images_are_equal(word_cloud_1, word_cloud_2))
+
+    
+    @settings(deadline=None)
+    @given(words=words_with_repeats_strategy(), seeds=st.sets(st.integers(), min_size=2, max_size=2))    
+    def test_word_cloud_seed_differences(self, words: list[str], seeds: int):
+        """
+        Test that two word clouds with different seeds are different
+        """
+        
+        seed1, seed2 = seeds
+
+        word_cloud_1 = make_word_cloud(words, seed=seed1)
+        word_cloud_2 = make_word_cloud(words, seed=seed2)
+
+
+        self.assertFalse(_two_images_are_equal(word_cloud_1, word_cloud_2))
